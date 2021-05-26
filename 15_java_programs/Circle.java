5502dab0302ffb0ef5265d1c31d60a7c4192e4f1
Sivapriyak56 <79069851+Sivapriyak56@users.noreply.github.com>
2021-05-26 02:40:17 -0400

    Add files via upload

---
+++ 15_java_programs/Circle.java
@@ -0,0 +1,10 @@
+public class Circle{
+public static void main(String[] args)
+{
+double r=7.5;
+double perimeter = (2)*(3.14)*(7.5);
+double area = (3.14)*(r)*(r);
+System.out.println("Perimeter: "+ perimeter);
+System.out.println("Area: "+ area);
+}
+}
