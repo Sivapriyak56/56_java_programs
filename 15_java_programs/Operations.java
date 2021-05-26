36a11caa4e3e089f32021e6f3dfe967652925719
Sivapriyak56 <79069851+Sivapriyak56@users.noreply.github.com>
2021-05-26 02:16:51 -0400

    commit

---
+++ 15_java_programs/Operations.java
@@ -0,0 +1,13 @@
+public class Operations{
+public static void main(String[] args)
+{
+int a = -5 + 8*6;
+int b = (55+9)%9;
+int c=20+-3*5/8;
+int d= 5+15/3*2-8%3;
+System.out.println("a: " + a);
+System.out.println("b: " + b);
+System.out.println("c: " + c);
+System.out.println("d: " + d);
+}
+}
---
+++ 15_java_programs/Sum.java
@@ -0,0 +1,7 @@
+public class Sum{
+public static void main(String[] args)
+{
+int sum = 5 + 3;
+System.out.println("sum: " + sum);
+}
+}
