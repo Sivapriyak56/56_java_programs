47d4b00bf493c13eae58259f77a3ffa243cbc90d
Sivapriyak56 <79069851+Sivapriyak56@users.noreply.github.com>
2021-05-27 12:05:17 -0400

    commit

---
+++ 15_java_programs/Sum_avg.java
@@ -0,0 +1,18 @@
+//8.Program to find sum and average of 5 numbers
+
+public class Sum_avg{
+public static void main(String[] args)
+{
+int n = 5;
+int a =2;
+int b =4;
+int c =6;
+int d =8;
+int e =10;
+ int sum;
+sum = a+b+c+d+e;
+int avg = sum/n;
+System.out.println("sum is:" + sum);
+System.out.println("average is:" + avg);
+}
+}
