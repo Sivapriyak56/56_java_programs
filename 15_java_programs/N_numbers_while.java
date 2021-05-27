f2639f16039b0f8d91d08edacce77f79931302ec
Sivapriyak56 <79069851+Sivapriyak56@users.noreply.github.com>
2021-05-27 12:03:06 -0400

    commit

---
+++ 15_java_programs/N_numbers_while.java
@@ -0,0 +1,15 @@
+//10.java program to print natural numbers up to 10 using while loop
+
+
+
+public class N_numbers_while{
+public static void main(String args[])
+{
+int num = 1;
+while (num <= 10)
+{
+System.out.println(num);
+num = num + 1;
+}
+}
+}
---
+++ 15_java_programs/Number.java
@@ -0,0 +1,16 @@
+//Java progtram to print wheather a number s positive or negative
+
+public class Number{
+public static void main( String args[])
+{
+int num = -12;
+if(num > 0)
+{
+System.out.println( "Number is positive");
+}
+else
+{
+System.out.println("Number is negative");
+}
+}
+}
