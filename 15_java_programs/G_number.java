f5090c4febc75cdd692d8d911c3c2fe1ee20314f
Sivapriyak56 <79069851+Sivapriyak56@users.noreply.github.com>
2021-05-27 12:01:54 -0400

    commit

---
+++ 15_java_programs/G_number.java
@@ -0,0 +1,27 @@
+public class G_number{
+public static void main(String args[])
+{
+int a = 10;
+int b = 7;
+int c = 25;
+if(a > b)
+{
+if(a > c)
+{
+System.out.println("greater  number is : " + a);
+}
+else{
+System.out.println("greater  number is : " + c);
+}
+}
+
+else if( b > c)
+{
+System.out.println("greater  number is : " + b);
+}
+else
+{
+System.out.println("greater  number is : " + c);
+}
+}
+}
