198d93fc014459a2964b0c7aade2ac52ed4fafca
Sivapriyak56 <79069851+Sivapriyak56@users.noreply.github.com>
2021-06-03 10:17:31 -0400

    commit

---
+++ Foreach.java
@@ -0,0 +1,10 @@
+//13. write a java program to print array element using for each loop
+
+public class Foreach{
+  public static void main(String[] args) {
+    String[] shop = {"Phone", "Laptop", "Computer", "Keyboard"};
+    for (String i : shop){
+      System.out.println(i);
+    }    
+  }
+}
